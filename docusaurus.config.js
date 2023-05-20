a4069a8eb378c3e432612b67733717e6e578687a
Rune Morling <ermo@serpentos.com>
2023-05-20 17:55:00 +0200

    Using help.getsol.us CNAME => baseUrl: '/'
    Signed-off-by: Rune Morling <ermo@serpentos.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,6 +14,7 @@ const config = {
   url: 'https://help.getsol.us',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
+  // Since we use the help.getsol.us CNAME for GH Pages, this is '/' for now
   baseUrl: '/',
 
   // GitHub pages deployment config.
