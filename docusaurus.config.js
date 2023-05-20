cd4f7d527a3fe521849505ca9df0b9909f50a23b
Joey Riches <josephriches@gmail.com>
2023-05-20 16:40:19 +0100

    config: change baseurl again for custom domain

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://help.getsol.us',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/help-center-docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
