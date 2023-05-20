56d762d59017df238cffd08f2cef65d594676978
Joey Riches <josephriches@gmail.com>
2023-05-20 12:47:56 +0100

    src: Steal homepage from serpent

---
+++ src/components/DocSections/index.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
+import Link from '@docusaurus/Link';
+import { IconUser, IconPackage } from '@tabler/icons-react';
+
+type DocSection = {
+  title: string;
+  description: JSX.Element;
+  link: string;
+  img: JSX.Element,
+};
+
+const DocList: DocSection[] = [
+  {
+    title: 'Help Center Home',
+    description: (
+      <>
+        Installing, software, boot mangement, troubleshooting and more
+      </>
+    ),
+    link: 'docs/intro',
+    img: (
+      <IconUser size="96" stroke="1"></IconUser>
+    )
+  },
+  {
+    title: 'Packaging',
+    description: (
+      <>
+        Get to grips with our advanced packaging features using easy to follow guides
+      </>
+    ),
+    link: 'docs/packaging',
+    img: (
+      <IconPackage size="96" stroke="1"></IconPackage>
+    )
+  },
+];
+
+function Doc({title, img, description, link}: DocSection) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        {img}
+      </div>
+        <div className="text--center padding-horiz--md">
+          <Link href={link}>
+            <h3>{title}</h3>
+          </Link>
+          <p>{description}</p>
+        </div>
+    </div>
+  );
+}
+
+export default function DocSections(): JSX.Element {
+  return (
+    <section className={styles.docSection}>
+      <div className="container">
+        <div className="row">
+          {DocList.map((props, idx) => (
+            <Doc key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+
+import styles from './index.module.css';
+import DocSections from '@site/src/components/DocSections';
+
+function DocsHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--secondary', styles.heroBanner)}>
+    <div className="container">
+    <h1 className="hero__title">Solus Help Center</h1>
+    <p className="hero__subtitle">Documentation for Solus</p>
+    </div>
+    </header>
+  );
+}
+
+export default function Docs(): JSX.Element {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+    title="Documentation"
+    description="Solus">
+    <DocsHeader />
+    <main>
+    <DocSections />
+    </main>
+    </Layout>
+  );
+}
