1d6e92abfb3e52a981bdf62c1b904d71683333df
Joshua Strobl <me@joshuastrobl.com>
2023-05-21 14:05:24 +0300

    drop tabler and use MUI

--- src/components/DocSections/index.tsx
+++ src/components/DocSections/index.tsx
@@ -1,55 +1,42 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './styles.module.css';
-import Link from '@docusaurus/Link';
-import { IconUser, IconPackage } from '@tabler/icons-react';
+import React from "react";
+import clsx from "clsx";
+import styles from "./styles.module.css";
+import Link from "@docusaurus/Link";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import Inventory2OutlinedIcon from "@mui/icons-material/Inventory2Outlined";
 
 type DocSection = {
   title: string;
   description: JSX.Element;
   link: string;
-  img: JSX.Element,
+  img: JSX.Element;
 };
 
 const DocList: DocSection[] = [
   {
-    title: 'Help Center Home',
-    description: (
-      <>
-        Installing, software, boot mangement, troubleshooting and more
-      </>
-    ),
-    link: 'docs/user/intro',
-    img: (
-      <IconUser size="96" stroke="1"></IconUser>
-    )
+    title: "Help Center Home",
+    description: <>Installing, software, boot mangement, troubleshooting and more</>,
+    link: "docs/user/intro",
+    img: <PersonOutlineOutlinedIcon sx={{ fontSize: 96 }} />,
   },
   {
-    title: 'Packaging',
-    description: (
-      <>
-        Get to grips with our advanced packaging features using easy to follow guides
-      </>
-    ),
-    link: 'docs/packaging',
-    img: (
-      <IconPackage size="96" stroke="1"></IconPackage>
-    )
+    title: "Packaging",
+    description: <>Get to grips with our advanced packaging features using easy to follow guides</>,
+    link: "docs/packaging",
+    img: <Inventory2OutlinedIcon sx={{ fontSize: 96 }} />,
   },
 ];
 
-function Doc({title, img, description, link}: DocSection) {
+function Doc({ title, img, description, link }: DocSection) {
   return (
-    <div className={clsx('col col-3')}>
-      <div className="text--center">
-        {img}
+    <div className={clsx("col col-3")}>
+      <div className="text--center">{img}</div>
+      <div className="text--center padding-horiz--md">
+        <Link href={link}>
+          <h3>{title}</h3>
+        </Link>
+        <p>{description}</p>
       </div>
-        <div className="text--center padding-horiz--md">
-          <Link href={link}>
-            <h3>{title}</h3>
-          </Link>
-          <p>{description}</p>
-        </div>
     </div>
   );
 }
