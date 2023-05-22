ac16a8f4a3a5c1fe78c90bd8499c931602de82a6
Joey Riches <josephriches@gmail.com>
2023-05-22 11:36:48 +0100

    config: Fix 'Edit This Page' url, resolves #328

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,7 +42,7 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/getsolus/help-center-docs/tree/master/",
         },
         blog: {
           showReadingTime: true,
