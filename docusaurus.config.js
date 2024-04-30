f2cb54d4bc7a216d86146b3f135379517b05abca
David Harder <david@davidjharder.ca>
2024-04-30 18:08:24 -0500

    Run prettier

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -225,7 +225,7 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
-        additionalLanguages: ['bash'],
+        additionalLanguages: ["bash"],
       },
     }),
 };
