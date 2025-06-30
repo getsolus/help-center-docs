592c4c1007d06296068a0b33434cae6f66808c35
Evan Maddock <maddock.evan@vivaldi.net>
2025-06-30 16:14:39 -0400

    editions: Split edition definitions to own file
    Right now, the data for our website is a bit spread out. People and teams live in `src/data`, but others like our editions and top-level categories live in the component and page files. Let's make everything easier to find. This moves the edition definitions to a file under `src/data`, making the component responsible only for the actual UI of the site.
    
    ~~I did this all jumbled up with https://github.com/getsolus/help-center-docs/pull/641, so it might be a good idea to merge this PR *after* that one, just in case I missed something when trying to separate all of my changes.~~
    
    Depends on https://github.com/getsolus/help-center-docs/pull/660
    
    Signed-off-by: Evan Maddock <maddock.evan@vivaldi.net>

--- src/components/editions/index.tsx
+++ src/components/editions/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+
 import clsx from "clsx";
+
 import Translate, { translate } from "@docusaurus/Translate";
 import Link from "@docusaurus/Link";
 import Image from "@theme/IdealImage";
@@ -7,70 +9,15 @@ import Heading from "@theme/Heading";
 import { Button, ButtonGroup, Grid, Stack, useMediaQuery, useTheme } from "@mui/material";
 import SettingsIcon from "@mui/icons-material/Settings";
 import TipsAndUpdatesIcon from "@mui/icons-material/TipsAndUpdates";
-
 import useBaseUrl from "@docusaurus/useBaseUrl";
 
-const Editions = [
-  {
-    name: "Budgie",
-    url: "budgie",
-    urlConfig: "budgie/configuration",
-    urlTips: "budgie/tips-and-tricks",
-    description: (
-      <Translate id="edition.budgie.description">
-        A feature-rich, luxurious desktop using the most modern technologies.
-      </Translate>
-    ),
-  },
-  {
-    name: "Plasma",
-    url: "plasma",
-    urlConfig: "plasma/configuration",
-    urlTips: "plasma/tips-and-tricks",
-    description: (
-      <Translate id="edition.plasma.description">
-        A sophisticated desktop experience for the tinkerers. Simple by default, powerful when needed.
-      </Translate>
-    ),
-  },
-  {
-    name: "GNOME",
-    url: "gnome",
-    urlConfig: "gnome/configuration",
-    urlTips: "gnome/tips-and-tricks",
-    description: (
-      <Translate id="edition.gnome.description">A simple, streamlined desktop for more modern hardware.</Translate>
-    ),
-  },
-  {
-    name: "MATE",
-    url: "mate",
-    urlConfig: "mate/configuration",
-    urlTips: "mate/tips-and-tricks",
-    description: (
-      <Translate id="edition.mate.description">A traditional desktop for advanced users and older hardware.</Translate>
-    ),
-  },
-  {
-    name: "Xfce",
-    url: "xfce",
-    urlConfig: "xfce/configuration",
-    urlTips: "xfce/tips-and-tricks",
-    description: (
-      <Translate id="edition.xfce.description">
-        A lightweight desktop that aims to be fast while still being friendly.
-      </Translate>
-    ),
-  },
-];
+import FaSettingsIcon from "@mui/icons-material/Settings";
+import FaTipsAndUpdatesIcon from "@mui/icons-material/TipsAndUpdates";
 
-type EditionCardProps = {
-  name: string;
-  url: string;
-  urlConfig: string;
-  urlTips: string;
-  description: JSX.Element;
-};
+import Editions from "@site/src/data/editions";
+import { Edition } from "@site/src/types";
+
+type EditionCardProps = Edition;
 
 const EditionCard = ({ name, url, urlConfig, urlTips, description }: EditionCardProps) => {
   const theme = useTheme();
@@ -78,7 +25,7 @@ const EditionCard = ({ name, url, urlConfig, urlTips, description }: EditionCard
   const image = useBaseUrl(`/img/${name}.jpg`);
 
   return (
-    <Grid xs={6}>
+    <Grid size={{ xs: 6 }}>
       <Stack className={clsx("card")} height={1}>
         <Stack className={clsx("card__image")}>
           <Link to={url}>
@@ -107,14 +54,14 @@ const EditionCard = ({ name, url, urlConfig, urlTips, description }: EditionCard
           >
             <Button
               href={urlConfig}
-              startIcon={!hideIcons ? <SettingsIcon /> : undefined}
+              startIcon={!hideIcons ? <FaSettingsIcon /> : undefined}
               sx={{ borderRadius: "10px", paddingInline: 2 }}
             >
               <Translate id="edition.card.configuration">Configuration</Translate>
             </Button>
             <Button
               href={urlTips}
-              startIcon={!hideIcons ? <TipsAndUpdatesIcon /> : undefined}
+              startIcon={!hideIcons ? <FaTipsAndUpdatesIcon /> : undefined}
               sx={{ borderRadius: "10px", paddingInline: 2 }}
             >
               <Translate id="edition.card.tipsntricks">Tips & Tricks</Translate>
@@ -129,7 +76,7 @@ const EditionCard = ({ name, url, urlConfig, urlTips, description }: EditionCard
 export const EditionCardsRow = () => {
   return (
     <Grid container columns={{ xs: 6, sm: 6, md: 6, lg: 12 }} spacing={2}>
-      {Editions.map((edition) => (
+      {Editions.map((edition: Edition) => (
         <EditionCard key={edition.name} {...edition} />
       ))}
     </Grid>
