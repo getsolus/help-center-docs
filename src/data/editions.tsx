19eafb156936951d4421a3d74ddc4defab140183
Evan Maddock <maddock.evan@vivaldi.net>
2025-11-28 22:18:39 -0500

    editions: Update editions for Solus 4.8
    Signed-off-by: Evan Maddock <maddock.evan@vivaldi.net>

--- src/data/editions.tsx
+++ src/data/editions.tsx
@@ -10,7 +10,8 @@ const Editions: Edition[] = [
     urlTips: "budgie/tips-and-tricks",
     description: (
       <Translate id="edition.budgie.description">
-        A feature-rich, luxurious desktop using the most modern technologies.
+        A feature-rich desktop using modern technologies. Customize your desktop with the dedicated Budgie Desktop
+        Settings app. Raven enables you to view notifications, control media playback and sound devices, and more!
       </Translate>
     ),
   },
@@ -21,7 +22,8 @@ const Editions: Edition[] = [
     urlTips: "plasma/tips-and-tricks",
     description: (
       <Translate id="edition.plasma.description">
-        A sophisticated desktop experience for the tinkerers. Simple by default, powerful when needed.
+        A sophisticated desktop experience for the tinkerers. We ship Plasma with a curated experience with sensible
+        defaults, enabling you to more quickly get to tinkering with the parts you love most.
       </Translate>
     ),
   },
@@ -31,16 +33,10 @@ const Editions: Edition[] = [
     urlConfig: "gnome/configuration",
     urlTips: "gnome/tips-and-tricks",
     description: (
-      <Translate id="edition.gnome.description">A simple, streamlined desktop for more modern hardware.</Translate>
-    ),
-  },
-  {
-    name: "MATE",
-    url: "mate",
-    urlConfig: "mate/configuration",
-    urlTips: "mate/tips-and-tricks",
-    description: (
-      <Translate id="edition.mate.description">A traditional desktop for advanced users and older hardware.</Translate>
+      <Translate id="edition.gnome.description">
+        A simple, streamlined desktop for more modern hardware. We ship GNOME with a variety of extensions, including
+        Speedinator and Appindicatorsupport. It's ready to go from the moment you start using it.
+      </Translate>
     ),
   },
   {
@@ -50,7 +46,8 @@ const Editions: Edition[] = [
     urlTips: "xfce/tips-and-tricks",
     description: (
       <Translate id="edition.xfce.description">
-        A lightweight desktop that aims to be fast while still being friendly.
+        A lightweight desktop that aims to be fast while still being friendly. We ship the Xfce Desktop to provide a
+        full-featured experience for users running less powerful hardware.
       </Translate>
     ),
   },
